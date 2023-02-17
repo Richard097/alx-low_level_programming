6b75afe29ba0035ba243db2c7666e7b40b2cd1d7
Richard <anyanwoucharichie@gmail.com>
2023-02-17 08:42:13 -0800

    4-puts commit update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#incluude <stdio.h>
+
 /**
- * main- A program that prints out string
- * Return: 0
+ * main- A program that prints a line
+ * Return:  Always 0 (success)
  */
+
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle\n");
-return (0);
+	puts("programming is like building a multilangual puzzle")		return (0);
 }
