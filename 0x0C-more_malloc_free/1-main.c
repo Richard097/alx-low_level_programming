b5d2fbbbca175bab0eb2a132133ca38573e9e50e
Richard <anyanwoucharichie@gmail.com>
2023-03-17 02:58:07 -0700

    update

---
+++ 0x0C-more_malloc_free/1-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char concat;
+
+	char *str_concat(char *s1, char *s2);
+
+	printf("%d\n", concat);
+	free(void *concat);
+	return (0);
+}
